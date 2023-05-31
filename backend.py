46af6d20212034ed62c4df3e7f511ee756d166f9
Shimon <shimonkocor@gmail.com>
2023-05-31 15:30:26 +0100

    Added sky images to match the forecast

---
+++ backend.py
@@ -0,0 +1,18 @@
+import requests
+
+API_KEY = "228f05be139272854304fb97f60ebf4a"
+
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    get_data(place="Tokyo")
+
